481080ab22208505b97eb4094164d919d66c225a
Sander Pick <sanderpick@gmail.com>
2023-07-16 19:47:48 -0700

    expand execute sql script
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- scripts/execute_sql.py
+++ scripts/execute_sql.py
@@ -1,20 +1,25 @@
+import argparse
 import itertools as itt
 import requests
 
-sql = "countries.sql"
-batch_size = 10
-start_sequence = 0
-# host = "http://34.125.163.42:26650"
-host = "http://127.0.0.1:26650"
+parser = argparse.ArgumentParser()
 
-with open(sql) as file:
+parser.add_argument("file", help="SQL file")
+parser.add_argument("-u", "--url", dest="url", default="http://127.0.0.1:26650", help="Host URL")
+parser.add_argument("-s", "--start-sequence", dest="sequence", default=0, help="Start sequence")
+parser.add_argument("-b", "--batch-size", dest="batch", default=10, help="Batch size")
+parser.add_argument("-g", "--gas-limit", dest="gas", default=10000000000, help="Gas limit")
+
+args = parser.parse_args()
+
+with open(args.file) as file:
     lines = file.readlines()
-    batches = [[e[1] for e in grp] for k, grp in itt.groupby(enumerate(lines), key=lambda x: x[0] // batch_size % 2)]
+    batches = [[e[1] for e in grp] for k, grp in itt.groupby(enumerate(lines), key=lambda x: x[0] // args.batch % 2)]
 
     for i, b in enumerate(batches):
         b = [s.rstrip() for s in b]
         stmts = ''.join(map(str, itt.chain.from_iterable(b)))
-        x = requests.post(host + "/v1/execute", json={'stmts': stmts,
-                                                      'sequence': start_sequence + i,
-                                                      'gas_limit': 10000000000000})
+        x = requests.post(args.url + "/v1/execute", json={'stmts': stmts,
+                                                          'sequence': args.sequence + i,
+                                                          'gas_limit': args.gas})
         print(x.json())
